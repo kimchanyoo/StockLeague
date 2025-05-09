769a83dd4f0b769840cd21ff8c4a74915e03ad85
Chanyoo Kim <cksdb012@gmail.com>
2025-05-09 18:16:05 +0900

    fix: record 변경으로 인한 getter 수정

--- src/main/java/com/stockleague/backend/auth/dto/request/RefreshTokenRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.stockleague.backend.auth.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RefreshTokenRequestDto(
-        @NotBlank(message = "리프레시 토큰은 필수입니다.")
-        String refreshToken
-) {
-}
