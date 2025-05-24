a297a552f74c326589a2cf711ea2f659f4903279
Chanyoo Kim <cksdb012@gmail.com>
2025-05-24 18:49:27 +0900

    fix: Enum 오류 수정

--- src/main/java/com/stockleague/backend/stock/dto/request/report/CommentReportRequestDto.java
+++ src/main/java/com/stockleague/backend/stock/dto/request/report/CommentReportRequestDto.java
@@ -3,10 +3,11 @@ package com.stockleague.backend.stock.dto.request.report;
 
 import com.stockleague.backend.stock.domain.Reason;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CommentReportRequestDto(
 
-        @NotBlank(message = "신고 사유는 비어 있을 수 없습니다.")
+        @NotNull(message = "신고 사유는 비어 있을 수 없습니다.")
         Reason reason,
 
         @NotBlank(message = "신고 상세 내용은 비어 있을 수 없습니다.")
