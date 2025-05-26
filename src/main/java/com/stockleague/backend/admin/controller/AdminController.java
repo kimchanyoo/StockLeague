6d2a94d651f2dc926d4730ad521bd6f7b6975d04
Chanyoo Kim <cksdb012@gmail.com>
2025-05-26 15:24:29 +0900

    refactor: 회원 이용 정지 Http메서드 변경 및 Swagger 수정

--- src/main/java/com/stockleague/backend/admin/controller/AdminController.java
+++ src/main/java/com/stockleague/backend/admin/controller/AdminController.java
@@ -14,7 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,8 +28,8 @@ public class AdminController {
 
     private final AdminService adminService;
 
-    @DeleteMapping("/users/{userId}")
-    @Operation(summary = "회원 강제 탈퇴", description = "관리자가 특정 회원을 이용 정지시킵니다.")
+    @PatchMapping("/users/{userId}")
+    @Operation(summary = "회원 이용 정지", description = "관리자가 특정 회원을 이용 정지시킵니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "회원 이용 정지 성공",
                     content = @Content(schema = @Schema(implementation = AdminUserForceWithdrawResponseDto.class),
