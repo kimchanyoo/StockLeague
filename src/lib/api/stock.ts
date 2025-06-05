776c945ef6fcbd063beb68266aa9ebde77c02b52
김석환 <tjrghks5939@gmail.com>
2025-06-05 20:02:23 +0900

    feat: 관심 api 연동 완료

--- src/lib/api/stock.ts
+++ src/lib/api/stock.ts
@@ -16,15 +16,53 @@ export interface GetTopStocksResponse {
   stocks: Stock[];
 };
 
+// 관심 종목 등록 성공 응답 타입
+export interface AddWatchlistSuccessResponse {
+  success: true;
+  message: string;
+  ticker: string;
+}
+
+// 관심 종목 목록 조회
+export interface WatchlistItem {
+  watchlistId: number;
+  stockId: number;
+  StockTicker: string;
+  StockName: string;
+}
+
+export interface DeleteWatchlistResponse {
+  success: boolean;
+  message: string;
+}
+
 // ─────────────────────────────
 // 종목 API
 // ─────────────────────────────
 
+// 종목
 export const getTopStocks = async (): Promise<GetTopStocksResponse> => {
   const res = await axiosInstance.get('/api/v1/stocks', {
-    headers: {
-      "Content-Type": "application/json",
-    },
   });
+  return res.data;
+};
+
+// 관심
+export const addWatchlist = async ( ticker: string ): Promise<AddWatchlistSuccessResponse> => {
+  const res = await axiosInstance.post('/api/v1/stock/watchlist', {
+    ticker,
+  });
+  return res.data;
+};
+
+// 관심 조회
+export const getWatchlist = async (): Promise<WatchlistItem[]> => {
+  const res = await axiosInstance.get("/api/v1/stock/watchlist");
+  return res.data.watchlists;
+};
+
+// 관심 삭제
+export const deleteWatchlist = async (watchlistId: number): Promise<DeleteWatchlistResponse> => {
+  const res = await axiosInstance.delete(`/api/v1/stock/watchlist/${watchlistId}`);
   return res.data;
 };
