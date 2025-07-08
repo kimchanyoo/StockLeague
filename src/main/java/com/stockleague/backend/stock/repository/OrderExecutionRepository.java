1b20b541870ce64dc0228102af0768fad059d880
Chanyoo Kim <cksdb012@gmail.com>
2025-07-08 14:49:50 +0900

    fix: Repository 오류 수정

--- src/main/java/com/stockleague/backend/stock/repository/OrderExecutionRepository.java
+++ src/main/java/com/stockleague/backend/stock/repository/OrderExecutionRepository.java
@@ -1,8 +1,9 @@
 package com.stockleague.backend.stock.repository;
 
+import com.stockleague.backend.stock.domain.OrderExecution;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderExecutionRepository extends JpaRepository<OrderExecutionRepository, Long> {
+public interface OrderExecutionRepository extends JpaRepository<OrderExecution, Long> {
 }
