8a67076941d722e5775ae73aac0aca7778643634
Chanyoo Kim <cksdb012@gmail.com>
2025-08-12 14:26:52 +0900

    refactor: STOMP 하트비트 수정

--- src/main/java/com/stockleague/backend/global/config/WebSocketConfig.java
+++ src/main/java/com/stockleague/backend/global/config/WebSocketConfig.java
@@ -2,9 +2,12 @@ package com.stockleague.backend.global.config;
 
 import com.stockleague.backend.global.interceptor.WebSocketSecurityInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -16,6 +19,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSocketSecurityInterceptor webSocketSecurityInterceptor;
 
+    @Bean
+    public TaskScheduler messageBrokerTaskScheduler() {
+        var ts = new ThreadPoolTaskScheduler();
+        ts.setPoolSize(1);
+        ts.setThreadNamePrefix("ws-heartbeat-");
+        ts.initialize();
+        return ts;
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
@@ -24,7 +36,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic", "/user/queue");
+        registry.enableSimpleBroker("/topic", "/user/queue")
+                .setHeartbeatValue(new long[]{15000, 15000})
+                .setTaskScheduler(messageBrokerTaskScheduler());
         registry.setUserDestinationPrefix("/user");
         registry.setApplicationDestinationPrefixes("/pub");
     }
